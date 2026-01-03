9b5f02de69f54700a4130bf2e01e2caa84a78b16
Eugene <amubeer@gmail.com>
2026-01-03 18:47:46 +0300

    working state

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ from pydantic.types import SecretStr
 
 class Config(BaseSettings):
     ROOTDIR: Path = Path(__file__).parent.parent
-    FILES_DIR: Path = ROOTDIR / "files"
+    FILES_DIR: Path = ROOTDIR / "files" # TODO: надо создавать вручную
     NETWORK_STORAGE: Path
     STORAGE_DIR: Path
 
